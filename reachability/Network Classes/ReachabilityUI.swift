1533bb87cb057d8f8ce45cb5d0b25819515ca415
Eric Dolecki <eric_dolecki@bose.com>
2019-06-06 08:37:32 -0400

    I added icons for wifi and cellular - the others get the same treatment. I also added cross fade for UIView as an extension. So changes are smoother and not abrupt in the UI.

--- reachability/Network Classes/ReachabilityUI.swift
+++ reachability/Network Classes/ReachabilityUI.swift
@@ -15,12 +15,26 @@ enum ConnectionState {
 class ReachabilityUI: UIView {
 
     var connectionState: ConnectionState = .NOT_CONNECTED
+    var myLabel: UILabel!
+    var myImage: UIImageView!
     
     init(state n: ConnectionState)
     {
         connectionState = n
-        super.init(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        super.init(frame: CGRect(x: 0, y: 0, width: 15, height: 15))
         self.layer.cornerRadius = self.frame.height / 2
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.borderWidth = 2
+        
+        myLabel = UILabel(frame: CGRect(x: 20, y: 0, width: 180, height: 15))
+        myLabel.font = UIFont.systemFont(ofSize: 10)
+        myLabel.textColor = UIColor.darkGray
+        self.addSubview(myLabel)
+        
+        myImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 15))
+        myImage.contentMode = .scaleAspectFit
+        self.addSubview(myImage)
+        
         evaluateConnectionState()
     }
     
@@ -32,16 +46,41 @@ class ReachabilityUI: UIView {
     
     private func evaluateConnectionState()
     {
+        myLabel.fadeTransition(0.3)
+        myImage.fadeTransition(0.3)
         if connectionState == .CELLULAR {
-            self.backgroundColor = UIColor.blue
+            self.backgroundColor = UIColor.clear
+            self.myLabel.text = "CELLULAR"
+            self.myImage.image = UIImage(named: "cellular")
+            self.layer.borderWidth = 0
         } else if connectionState == .WIFI {
-            self.backgroundColor = UIColor.orange
+            self.backgroundColor = UIColor.clear
+            self.myLabel.text = "WI-FI"
+            self.myImage.image = UIImage(named: "wifi")
+            self.layer.borderWidth = 0
         } else if connectionState == .ERROR {
             self.backgroundColor = UIColor.red
+            self.myLabel.text = "ERROR"
+            self.myImage.image = nil
+            self.layer.borderWidth = 2
         } else if connectionState == .NOT_CONNECTED {
             self.backgroundColor = UIColor.lightGray
+            self.myLabel.text = "..."
+            self.myImage.image = nil
+            self.layer.borderWidth = 2
         }
     }
     
     required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
+
+// Used for text and image fade transitioning.
+extension UIView {
+    func fadeTransition(_ duration: CFTimeInterval) {
+        let animation = CATransition()
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        animation.type = CATransitionType.fade
+        animation.duration = duration
+        layer.add(animation, forKey: CATransitionType.fade.rawValue)
+    }
 }
